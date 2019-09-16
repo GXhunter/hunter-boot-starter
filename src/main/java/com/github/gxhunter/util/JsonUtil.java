dd1ef9cf1f63ce269ca94c90aa70aa315b8f2f57
wanggx <215145411@qq.com>
2019-09-16 18:19:38 +0800

    harbor请求转发完成

--- src/main/java/com/github/gxhunter/util/JsonUtil.java
+++ src/main/java/com/github/gxhunter/util/JsonUtil.java
@@ -2,43 +2,42 @@ package com.github.gxhunter.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.*;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.*;
 import java.util.Collection;
 
 /**
  * @author hunter
  * @date 2018.6.21
  */
-public class JsonUtil{
+public class JsonUtil {
     private static ObjectMapper objectMapper = new ObjectMapper();
 
-    static{
+    static {
         //只序列化不为null的字段
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         //取消默认转换timestamps形式
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,false);
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         //忽略空Bean转json的错误
-        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS,false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         //忽略 在json字符串中存在，但是在java对象中不存在对应属性的情况。防止错误
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
 
     /**
      * 对象转json
      */
-    public static <T> String toJSON(T obj){
-        if(obj == null){
+    public static <T> String toJSON(T obj) {
+        if (obj == null) {
             return null;
         }
-        try{
+        try {
             return obj instanceof String ? (String) obj : objectMapper.writeValueAsString(obj);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -47,13 +46,13 @@ public class JsonUtil{
     /**
      * 对象转json数组
      */
-    public static <T> String toJSONPretty(T obj){
-        if(obj == null){
+    public static <T> String toJSONPretty(T obj) {
+        if (obj == null) {
             return null;
         }
-        try{
+        try {
             return obj instanceof String ? (String) obj : objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -67,26 +66,26 @@ public class JsonUtil{
      * @param <T>   java对象类型
      * @return
      */
-    public static <T> T parse(String str,Class<T> clazz){
-        if(StringUtils.isEmpty(str) || clazz == null){
+    public static <T> T parse(String str, Class<T> clazz) {
+        if (StringUtils.isEmpty(str) || clazz == null) {
             return null;
         }
 
-        try{
-            return clazz.equals(String.class) ? (T) str : objectMapper.readValue(str,clazz);
-        }catch(Exception e){
+        try {
+            return clazz.equals(String.class) ? (T) str : objectMapper.readValue(str, clazz);
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    public static <T> T parse(String str,TypeReference<T> typeReference){
-        if(StringUtils.isEmpty(str) || typeReference == null){
+    public static <T> T parse(String str, TypeReference<T> typeReference) {
+        if (StringUtils.isEmpty(str) || typeReference == null) {
             return null;
         }
-        try{
-            return (T) (typeReference.getType().equals(String.class) ? str : objectMapper.readValue(str,typeReference));
-        }catch(Exception e){
+        try {
+            return (T) (typeReference.getType().equals(String.class) ? str : objectMapper.readValue(str, typeReference));
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -102,14 +101,36 @@ public class JsonUtil{
      * @return 对象
      */
     @SafeVarargs
-    public static <T extends Collection<E>,E> T parse(String str,Class<T> collectionClass,Class<E>... elementClasses){
-        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(collectionClass,elementClasses);
-        try{
-            return objectMapper.readValue(str,javaType);
-        }catch(Exception e){
+    public static <T extends Collection<E>, E> T parse(String str, Class<T> collectionClass, Class<E>... elementClasses) {
+        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(collectionClass, elementClasses);
+        try {
+            return objectMapper.readValue(str, javaType);
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
+    public static JsonNode toJsonNode(String json) {
+        JsonNode jsonNode = null;
+        try {
+            jsonNode = objectMapper.readTree(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonNode;
+    }
+
+    public static JsonNode toJsonNode(Object object) {
+        JsonNode jsonNode = null;
+        try (
+                ByteOutputStream bos = new ByteOutputStream();
+                InputStream in = new ByteArrayInputStream(bos.getBytes());
+        ) {
+            jsonNode = objectMapper.readTree(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonNode;
+    }
 }
