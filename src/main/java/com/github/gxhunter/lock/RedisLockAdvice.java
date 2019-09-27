717170c011c86e9e5f7f832800ef14ff69241d58
wanggx <215145411@qq.com>
2019-09-27 17:09:16 +0800

    分布式锁注释级别改为info

--- src/main/java/com/github/gxhunter/lock/RedisLockAdvice.java
+++ src/main/java/com/github/gxhunter/lock/RedisLockAdvice.java
@@ -16,53 +16,50 @@ import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
  * @date 2019/5/27 15:31
  */
 @Slf4j
-public class RedisLockAdvice extends AbstractPointcutAdvisor implements MethodInterceptor{
+public class RedisLockAdvice extends AbstractPointcutAdvisor implements MethodInterceptor {
 
     private RedisDistributionLock redisDistributionLock;
 
-    public RedisLockAdvice(RedisDistributionLock redisDistributionLock){
+    public RedisLockAdvice(RedisDistributionLock redisDistributionLock) {
         this.redisDistributionLock = redisDistributionLock;
     }
 
     @Override
-    public Pointcut getPointcut(){
+    public Pointcut getPointcut() {
         return AnnotationMatchingPointcut.forMethodAnnotation(RedisLock.class);
     }
 
     @Override
-    public Advice getAdvice(){
+    public Advice getAdvice() {
         return this;
     }
 
     /**
      * 方法增强
-     *
-     * @param invocation
-     * @return
-     * @throws Throwable
      */
     @Override
-    public Object invoke(MethodInvocation invocation) throws Throwable{
-        log.debug("redis lock running");
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        long id = Thread.currentThread().getId();
+        log.info(id + "尝试获取锁...");
         long startTime = System.currentTimeMillis();
         String keyName = null;
         String lockValue = null;
-        try{
+        try {
             RedisLock redisLock = invocation.getMethod().getAnnotation(RedisLock.class);
-            keyName = redisDistributionLock.generateKeyName(invocation,redisLock);
-            while((lockValue = redisDistributionLock.lock(keyName,redisLock.expireTime())) == null){
-                if(startTime + redisLock.retryTimes() < System.currentTimeMillis()){
+            keyName = redisDistributionLock.generateKeyName(invocation, redisLock);
+            while ((lockValue = redisDistributionLock.lock(keyName, redisLock.expireTime())) == null) {
+                if (startTime + redisLock.retryTimes() < System.currentTimeMillis()) {
 //                    超时
-                    throw new RedisLockException("获取锁超时,等待时间："+redisLock.retryTimes()+"毫秒");
+                    throw new RedisLockException("获取锁超时,等待时间：" + redisLock.retryTimes() + "毫秒");
                 }
                 Thread.sleep(50);
             }
-            log.debug("lock has bean got.method process");
+            log.info(id + "成功获取到锁");
             return invocation.proceed();
-        }finally{
-            if(StringUtils.isNoneBlank(keyName,lockValue)){
-                if(!redisDistributionLock.unlock(keyName,lockValue)){
-                    log.error("redis unlock failed,the key is {},value is {}.",keyName,lockValue);
+        } finally {
+            if (StringUtils.isNoneBlank(keyName, lockValue)) {
+                if (!redisDistributionLock.unlock(keyName, lockValue)) {
+                    log.error("redis 解锁失败,请检查是否已超时自动释放,key ： {},value ： {}.", keyName, lockValue);
                 }
             }
         }
